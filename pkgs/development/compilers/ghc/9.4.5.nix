94c4a7fc2cd1c036f702cc62804991137dd8ba48
Jappie Klooster <jappieklooster@hotmail.com>
2023-04-05 19:29:52 +0200

    fix syntax

--- pkgs/development/compilers/ghc/9.4.5.nix
+++ pkgs/development/compilers/ghc/9.4.5.nix
@@ -208,8 +208,8 @@ stdenv.mkDerivation (rec {
     })
 
     (fetchpatch {
-      name = "";
-      url = "https://gitlab.haskell.org/ghc/ghc/-/commit/fe575506dbc37a550df97fd5e3a4b1ee70a140a4.patch"
+      name = "constraint-solver-patch.patch";
+      url = "https://gitlab.haskell.org/ghc/ghc/-/commit/fe575506dbc37a550df97fd5e3a4b1ee70a140a4.patch";
       sha256 = "000hfamr16w8gch0lgln2912r8aryjky1hfcda3jkcwa5cdzgjdv";
     })
 
