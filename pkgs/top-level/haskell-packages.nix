822a218234f9c8806732a16279d6f35b01128c8d
Jappie Klooster <jappieklooster@hotmail.com>
2023-04-05 16:23:13 +0200

    add, based on 9.4.4 and with the constraint solver patch

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -21,6 +21,7 @@ let
     "ghc92"
     "ghc942"
     "ghc943"
+    "ghc945"
     "ghc94"
     "ghcHEAD"
   ];
@@ -34,6 +35,7 @@ let
     "ghc94"
     "ghc942"
     "ghc943"
+    "ghc945"
     "ghcHEAD"
   ];
 
@@ -211,6 +213,30 @@ in {
       llvmPackages = pkgs.llvmPackages_12;
     };
     ghc943 = callPackage ../development/compilers/ghc/9.4.3.nix {
+      bootPkgs =
+        # Building with 9.2 is broken due to
+        # https://gitlab.haskell.org/ghc/ghc/-/issues/21914
+        # Use 8.10 as a workaround where possible to keep bootstrap path short.
+
+        # On ARM text won't build with GHC 8.10.*
+        if stdenv.hostPlatform.isAarch then
+          # TODO(@sternenseemann): package bindist
+          packages.ghc902
+        # No suitable bindists for powerpc64le
+        else if stdenv.hostPlatform.isPower64 && stdenv.hostPlatform.isLittleEndian then
+          packages.ghc902
+        else
+          packages.ghc8107Binary;
+      inherit (buildPackages.python3Packages) sphinx;
+      # Need to use apple's patched xattr until
+      # https://github.com/xattr/xattr/issues/44 and
+      # https://github.com/xattr/xattr/issues/55 are solved.
+      inherit (buildPackages.darwin) xattr autoSignDarwinBinariesHook;
+      # Support range >= 10 && < 14
+      buildTargetLlvmPackages = pkgsBuildTarget.llvmPackages_12;
+      llvmPackages = pkgs.llvmPackages_12;
+    };
+    ghc945 = callPackage ../development/compilers/ghc/9.4.5.nix {
       bootPkgs =
         # Building with 9.2 is broken due to
         # https://gitlab.haskell.org/ghc/ghc/-/issues/21914
