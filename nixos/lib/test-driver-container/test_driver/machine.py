182b5526c0435660704afcd12fe651be50dd9fa3
Jappie Klooster <jappieklooster@hotmail.com>
2023-08-16 17:52:00 -0400

    add open port

--- nixos/lib/test-driver-container/test_driver/machine.py
+++ nixos/lib/test-driver-container/test_driver/machine.py
@@ -5,5 +5,8 @@ class Machine:
     def wait_for_console_text(self, arg: str) -> None:
         pass
 
+    def wait_for_open_port(self, arg: int) -> None:
+        pass
+
     def succeed(self, arg: str) -> None:
         pass
