55becc45676ef3aab5d23a3fe337a67ea03fbba7
Jappie Klooster <jappieklooster@hotmail.com>
2023-08-16 17:47:11 -0400

    fix formatting

--- nixos/lib/test-driver-container/test_driver/machine.py
+++ nixos/lib/test-driver-container/test_driver/machine.py
@@ -1,2 +1,9 @@
 class Machine:
-    pass
+    def wait_for_unit(self, arg: str) -> None:
+        pass
+
+    def wait_for_console_text(self, arg: str) -> None:
+        pass
+
+    def succeed(self, arg: str) -> None:
+        pass
