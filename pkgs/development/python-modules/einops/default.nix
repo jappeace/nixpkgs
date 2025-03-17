c8a3afa8c17b8abba76322afbcb5f5f6342f5067
Jappie Klooster <jappieklooster@hotmail.com>
2025-03-17 12:59:23 +0100

    einops: 0.6.0 -> 0.8.0
    also drop the uneaded dependency jupyter,
    which makes this build massive

--- pkgs/development/python-modules/einops/default.nix
+++ pkgs/development/python-modules/einops/default.nix
@@ -3,7 +3,6 @@
 , chainer
 , fetchFromGitHub
 , hatchling
-, jupyter
 , keras
   #, mxnet
 , nbconvert
@@ -17,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "einops";
-  version = "0.6.0";
+  version = "0.8.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -26,14 +25,13 @@ buildPythonPackage rec {
     owner = "arogozhnikov";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-/bnp8IhDxp8EB/PoW5Dz+7rOru0/odOrts84aq4qyJw=";
+    hash = "sha256-/00p8IhDxp8EB/PoW5Dz+7rOru0/odOrts84aq4qyJw=";
   };
 
   nativeBuildInputs = [ hatchling ];
 
   nativeCheckInputs = [
     chainer
-    jupyter
     keras
     # mxnet (has issues with some CPUs, segfault)
     nbconvert
